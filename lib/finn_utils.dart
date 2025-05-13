776ce9c0a4966808ef9e944984ef8602ccf36c8f
Vivian Dee <83787516+Jesse-Dan@users.noreply.github.com>
2025-05-13 08:43:06 +0100

    [add]: getters

--- lib/finn_utils.dart
+++ lib/finn_utils.dart
@@ -1,5 +1,13 @@
+import 'package:finn_utils/utils/finn_util_storage/finn_util_storage.dart';
+
 export 'utils/client/client.dart';
+export 'utils/client/helpers/settle.dart';
 export 'utils/exceptions/finn_utils_client_exceptions.dart';
 export 'utils/exceptions/finn_utils_settler_exception.dart';
 export 'utils/finn_util_storage/finn_util_storage.dart';
-export 'utils/client/helpers/settle.dart';
+
+abstract class FinnUtils {
+  static FinnUtilStorage get storage => FinnUtilStorage.instance;
+
+  static Future<void> get initStorage => FinnUtilStorage.initialize();
+}
