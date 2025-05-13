6c737bb3dd1364d0737beb8820dadc95d91b10ae
Vivian Dee <83787516+Jesse-Dan@users.noreply.github.com>
2025-05-13 01:08:44 +0100

    [add]: most recent findings on exposing settler

--- lib/finn_utils.dart
+++ lib/finn_utils.dart
@@ -2,3 +2,4 @@ export 'utils/client/client.dart';
 export 'utils/exceptions/finn_utils_client_exceptions.dart';
 export 'utils/exceptions/finn_utils_settler_exception.dart';
 export 'utils/finn_util_storage/finn_util_storage.dart';
+export 'utils/client/helpers/settle.dart';
